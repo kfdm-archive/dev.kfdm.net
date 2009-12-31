2c6198a17c3ac14ab03075907c1e3d7248b1765b
Paul Traylor <paul@wegame.com>
2009-12-30 22:47:31 -0800

    Initial style pass

--- application/views/footer.php
+++ application/views/footer.php
@@ -1,5 +1,9 @@
-		<footer>
-			Footer goes here
-		</footer>
+			</div>
+			<footer>
+				<div>
+					Copyright &copy; Paul Traylor 2004-<?=date('Y')?>
+				</div>
+			</footer>
+		</div>
 	</body>
 </html>
--- application/views/header.php
+++ application/views/header.php
@@ -1,19 +1,27 @@
 <!DOCTYPE html>
 <html>
 	<head>
-		<title>dev.kfdm.net</title>
 		<meta charset="utf-8">
+		<title>dev.kfdm.net</title>
+		<!--[if IE]>
+			<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+		<![endif]-->
+		<link rel="stylesheet" href="/style.css" media="screen" />
 	</head>
-	<body>
-		<nav>
-			<ul>
-				<li><a href="/gallery/">Gallery</a>
-				<li><a href="/quotes/">Quotes</a></li>
+	<body class="<?=strtolower( substr( get_class($this),0,-11) ) ?>">
+		<div id="container">
+			<nav>
+				<div>
+					<h1>dev.kfdm.net</h1>
+					<ul>
+						<li><a href="/gallery/">Gallery</a></li>
+						<li><a href="/quotes/">Quotes</a></li>
 <?php if(Auth::instance()->logged_in()):?>
-				<li><a href="/logout/">Logout (<?=Auth::instance()->get_user()->username?>)</a></li>
+						<li><a href="/logout/">Logout (<?=Auth::instance()->get_user()->username?>)</a></li>
 <?php else:?>
-				<li><a href="/login/?next=/<?=url::current()?>/">Login</a></li>
+						<li><a href="/login/?next=/<?=url::current()?>/">Login</a></li>
 <?php endif;?>
-			</ul>
-		</nav>
-		
+					</ul>
+				</div>
+			</nav>
+			<div id="content">
