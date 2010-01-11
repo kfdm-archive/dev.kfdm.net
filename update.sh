9f58ed2dcef388e39f3ac9836d7032d642125f1f
Paul Traylor <paul@wegame.com>
2010-01-10 19:54:24 -0800

    Update script changes

--- update.sh
+++ update.sh
@@ -2,11 +2,10 @@
 
 #Change script to the location of this file
 script_path=`dirname $0`
-echo "---Moving to ($script_path)---"
+#echo "---Moving to ($script_path)---"
 cd $script_path
 
 #Update From Git
 echo "---Update Master---"
-git stash
 git pull --rebase origin master
-git stash apply
+php bin/clear_tmp_gallery.php
